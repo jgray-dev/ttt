7ec1d0358b36edf821e7b5d2ec214cdb7a6c3212
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-06 20:22:26 +0000

    Feat: Add account dropdown to Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import {useState} from "react";
-import {BrowserView, MobileView, isBrowser, isMobile} from 'react-device-detect';
+import {FaUserAlt} from "react-icons/fa";
+import Account from "./Account.jsx";
 
 
 export default function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Add a state to track if the user is logged in or not
+  const user = "astro"
+  const [accountOpen, setAccountOpen] = useState(false)
+  const [accountPage, setAccountPage] = useState(<></>)
+
+  function handleClick() {
+    if (accountOpen) { // Clicked on account button again.. Hide page
+      setAccountPage(<></>)
+    } else {
+      setAccountPage(<Account/>)
+    }
+    setAccountOpen(!accountOpen)
+  }
 
   const handleLogin = () => {
     // Add login logic here, e.g., redirect to a login page or show a modal
@@ -16,48 +28,32 @@ export default function Navbar() {
   };
 
   return (
-    <div className="fixed top-0 w-full h-16 bg-black/50 flex justify-between items-center px-4">
-      <a href="/home">
-        <img
-          src={`https://mynameisnt.kim/uploads/images/ttt.png`}
-          alt="logo"
-          height="80"
-          width="350"
-          className="ml-1 md:block hidden"
-        />
-        <img
-          src={`https://mynameisnt.kim/uploads/images/ttt_mobile.png`}
-          alt="logo"
-          height="80"
-          width="40"
-          className="m-1.5 md:hidden block"
-        />
-      </a>
-      <div>
-        {isLoggedIn ? (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setIsLoggedIn(false)}
-          >
-            Logout
-          </button>
-        ) : (
-          <>
-            <button
-              className="bg-pink-950-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              onClick={handleLogin}
-            >
-              Sign In
-            </button>
-            <button
-              className="bg-pink-950-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              onClick={handleSignup}
-            >
-              Sign Up
-            </button>
-          </>
-        )}
+    <div className="fixed h-screen w-screen">
+      <div className="fixed top-0 w-full h-16 bg-black/50 flex justify-between items-center px-4">
+        <a href="/home">
+          <img
+            src={`https://mynameisnt.kim/uploads/images/ttt.png`}
+            alt="logo"
+            height="80"
+            width="350"
+            className="ml-1 md:block hidden"
+          />
+          <img
+            src={`https://mynameisnt.kim/uploads/images/ttt_mobile.png`}
+            alt="logo"
+            height="80"
+            width="40"
+            className="m-1.5 md:hidden block"
+          />
+        </a>
+        <div className="cursor-pointer p-4"
+             onClick={() => {
+               handleClick()
+             }}>
+          <FaUserAlt className={"fill-white h-6 w-6"}/>
+        </div>
       </div>
+      {accountPage}
     </div>
   );
 }
