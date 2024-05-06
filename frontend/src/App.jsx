50793669d3bc23473e9688552a75fa4bc5d8d58a
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-06 19:16:52 +0000

    Feat: Implement thread listing and navigation on the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,46 +2,52 @@ import './index.css'
 import Backdrop from './components/Backdrop/Backdrop.jsx'
 import Navbar from './components/Navbar.jsx'
 import {useState, useEffect} from 'react'
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Accounts from "./pages/Accounts.jsx";
-import Guidlines from "/Users/donjuan/Downloads/projects/ttt/frontend/src/pages/Guidelines.jsx";
-import Threads from "./pages/Threads.jsx";
+import Guidelines from "./pages/Guidelines.jsx";
+import Thread from "./pages/Thread.jsx";
 
-
-
+export const url = 'http://67.164.191.36:4000/api'
 
 function App() {
-    const [width, setWidth] = useState(window.innerWidth);
+  const [threads, setThreads] = useState()
 
+  useEffect(() => {
+    console.log(`${url}/threads`);
+    fetch(`${url}/threads`)
+      .then(r => r.json())
+      .then(data => {
+        console.log(data);
+        const sortedThreads = data.sort((a, b) => b.last_updated - a.last_updated);
+        setThreads(sortedThreads);
+      });
+  }, []);
 
-    // set isMobile to true/false depending on if screen size is > or < than 768
-    function handleWindowSizeChange() {
-        setWidth(window.innerWidth);
-    }
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange);
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange);
-        }
-    }, []);
-    const isMobile = width <= 768;
-
-    return (
-        <div className = "App">
-            <Home />
-            <Accounts />
-            <Guidlines />
-            <Threads />
-            <Footer />
-            <Navbar />
-            <Search />
+  return (
+    <div className="w-screen h-screen text-black">
+      <BrowserRouter>
+        <Backdrop/>
+        <div className="fixed top-16 w-full h-full bg-black/25">
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home"/>}/>
+            <Route path="/home" element={<Home threads={threads}/>}/>
+            <Route path="/account" element={<Accounts/>}/>
+            <Route path="/guidelines" element={<Guidelines/>}/>
+            <Route element={
+              <div>
+                {threads ? threads.map(thread => (
+                  <Link key={thread.id} to={`/thread/${thread.id}`}>{thread.title}</Link>
+                )) : <></>}
+              </div>
+            }/>
+            <Route path="/thread/:id" element={<Thread/>}/>
+          </Routes>
         </div>
-
-    );
+      </BrowserRouter>
+    </div>
+  )
 }
-    export default App;
 
-
-
-    // test for branch
+export default App;
