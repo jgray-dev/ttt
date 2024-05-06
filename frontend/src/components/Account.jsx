4a781bf1f71ae592cd51a423ff06588b595e5c1b
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-06 20:38:12 +0000

    Implement Account component with close functionality

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,12 +1,20 @@
-export default function Account() {
+import {useState} from "react";
+
+export default function Account({hideAccount}) {
   return (
-    <div className="fixed w-full h-full bg-black/30`" onClick={() => console.log("Outside")}>
-      <div className="mt-12 h-[80%] w-[80%] bg-black mx-auto" onClick={(event) => {
-        event.stopPropagation();
-        console.log("Inside");
-      }}>
+    <div className={`fixed w-full h-full bg-black/30 `}
+         onClick={() => {
+           hideAccount()
+         }}>
+      <div
+        className="mt-12 h-[80%] w-[80%] bg-black mx-auto"
+        onClick={(event) => {
+          event.stopPropagation();
+          console.log("Inside");
+        }}
+      >
         meow
       </div>
     </div>
-  )
+  );
 }
