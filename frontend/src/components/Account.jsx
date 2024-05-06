a918f9e23cfab7bc4df0a7d2c4011de07829c261
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-06 20:22:25 +0000

    Implement Account component with click event handling

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,3 +1,12 @@
 export default function Account() {
-  return (<div>account box here</div>)
+  return (
+    <div className="fixed w-full h-full bg-black/30`" onClick={() => console.log("Outside")}>
+      <div className="mt-12 h-[80%] w-[80%] bg-black mx-auto" onClick={(event) => {
+        event.stopPropagation();
+        console.log("Inside");
+      }}>
+        meow
+      </div>
+    </div>
+  )
 }
