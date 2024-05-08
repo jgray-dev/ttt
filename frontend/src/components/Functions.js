28cb0760d2a17a9cd88d4a251c0a517a0e730bd2
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-08 19:36:10 +0000

    feat: Add cookie management functions to Frontend

--- frontend/src/components/Functions.js
+++ frontend/src/components/Functions.js
@@ -32,3 +32,51 @@ export function getTime(updatedTime) {
     return years === 1 ? "1 year" : `${years} years`;
   }
 }
+
+//Return our cookies as an object
+export function readCookie() {
+  let cookies = document.cookie.split("; ");
+  let cookieObject = {};
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].split("=");
+    cookieObject[cookie[0]] = cookie[1];
+  }
+  return cookieObject;
+}
+
+
+//Debug to print all our cookies
+export function printAllCookies() {
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    if (name !== "undefined") {
+      console.log(cookie)
+    }
+  }
+}
+
+//Deletes all saved cookies
+export function deleteAllCookies() {
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    if (name !== "undefined") {
+      document.cookie =
+        name + "=; SameSite=Strict; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  }
+}
+
+//Sets our cookie
+export function updateCookie(username) {
+  const date = new Date();
+  date.setFullYear(date.getFullYear() + 1);
+  document.cookie = `username=${username}; SameSite=Strict; Expires=${date.toUTCString()}`;
+}
+
+
